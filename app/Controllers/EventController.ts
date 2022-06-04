5a13197834e176d1c944d528209c9649a95de529
Angelo Lencina <angelolencina@gmail.com>
2022-06-04 12:29:28 -0300

    removido espera do evento de reserva

--- app/Controllers/EventController.ts
+++ app/Controllers/EventController.ts
@@ -2,7 +2,7 @@ import DeskoModule from '../../core/desko.module'
 
 export default class EventController {
   public async index({ request, response }) {
-    await new DeskoModule().event(request.body())
+    new DeskoModule().event(request.body())
     response.status(200)
   }
 }
