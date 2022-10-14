d6f0779c40f37b55ffb3c2ceb586f2c634459731
Angelo Lencina <angelo.lencina@ifood.com.br>
2022-10-14 09:04:59 -0300

    fix env

--- env.ts
+++ env.ts
@@ -21,6 +21,6 @@ export default Env.rules({
   APP_NAME: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'testing'] as const),
-  CONTROLID_FUNCTION_ACCESS_CONTROL: Env.schema.boolean.optional(),
-  CONTROLID_FUNCTION_QRCODE: Env.schema.boolean.optional(),
+  FUNCTION_ACCESS_CONTROL: Env.schema.boolean.optional(),
+  FUNCTION_QRCODE: Env.schema.boolean.optional(),
 })
