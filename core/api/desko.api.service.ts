aec2be114880ca5199f6f7a3b7595463be84135e
Angelo Lencina <angelolencina@gmail.com>
2023-03-23 11:56:27 -0300

    incluído homologação de controle de acesso para emails específicos

--- core/api/desko.api.service.ts
+++ core/api/desko.api.service.ts
@@ -1,10 +1,23 @@
 import { CheckInOutDto } from '../dto/desko.check-in-out.dto'
 import { PersonalBadgeDto } from '../dto/desko.personal-badge.dto'
 import { findOneBooking, checkinByUser, registerPersonalBadge } from '../../apis/deskbee.api'
+import Env from '@ioc:Adonis/Core/Env'
 
 export default class DeskoApiService {
   public async getBooking(uuid): Promise<object | boolean> {
-    return findOneBooking(uuid)
+    const emailsOnHomologation = this.getEmailsHomologation()
+     const booking = await findOneBooking(uuid)
+     if(emailsOnHomologation && Array.isArray(emailsOnHomologation)){
+          console.warn("Homologação de controle de acesso está ativa")
+          if(emailsOnHomologation?.includes(booking?.person?.email?.toLowerCase())){
+            console.warn(`Email ${booking?.person?.email?.toLowerCase()} ESTÁ dentro da homologação - Controle de acesso ATIVADO`)
+            return booking
+          }
+          console.warn(`Email ${booking?.person?.email?.toLowerCase()} NÃO ESTÁ dentro da homologação - Controle de acesso DESATIVADO`)
+          return null
+
+     }
+    return booking
   }
 
   public async sendPersonalBadge(personalBadgeDto: PersonalBadgeDto[]): Promise<any> {
@@ -13,5 +26,14 @@ export default class DeskoApiService {
 
   public async sendDeviceEvent(events: CheckInOutDto[]) {
     return checkinByUser(events)
+  }
+  private getEmailsHomologation(): string[] | boolean{
+    if(Env.get('NODE_ENV').toLowerCase() === 'homologation'){
+      const emails = Env.get('EMAIL_USERS_HOMOLOGATION')
+      return emails ? emails.toLowerCase().split(',') : []
+    }
+
+    return false
+
   }
 }
